c548f46f8d9954f4b9bd1c4062c606789d35d124
Yeshwanth <112691767+yesh02@users.noreply.github.com>
2022-12-15 23:51:20 +0530

    Add files via upload

---
+++ aws-live-master/config.py
@@ -0,0 +1,6 @@
+customhost = "employee2.cka3tc4n3juu.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "123456789"
+customdb = "employee"
+custombucket = "addemployee70"
+customregion = "us-east-1b"
